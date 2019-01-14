31a44d469fff22c69b88513c21532130e25a19ea
Moses <ysdxz207@qq.com>
2019-01-14 17:40:19 +0800

    版本

--- src/main/java/win/hupubao/common/utils/Md5Utils.java
+++ src/main/java/win/hupubao/common/utils/Md5Utils.java
@@ -29,7 +29,7 @@ public final class Md5Utils {
     private final static char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
             'F'};
 
-    public final static String md5(String str) {
+    public static String md5(String str) {
 
         try {
             byte[] btInput = (str).getBytes(StandardCharsets.UTF_8);
